733e2f4b657461387f39433297eecb5e5188aa85
Pastafredda <justspotify09@gmail.com>
2023-05-17 12:29:51 +0200

    colore e aumento dei likes

--- main.js
+++ main.js
@@ -55,3 +55,86 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const contaier= document.getElementById('container');
+
+
+posts.forEach(element => {
+    
+    const post =        
+        `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author.image}" alt="">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+
+    contaier.innerHTML += post;
+
+   
+});
+
+
+const counters = document.querySelectorAll(".js-likes-counter");
+console.log(counters);
+
+const likeButtons = document.querySelectorAll(".like-button");
+console.log(likeButtons);
+
+likeButtons.forEach((element, i) => {
+    element.addEventListener('click',
+        function(event){
+            event.preventDefault()
+            element.classList.add("like-button--liked");
+            let contatoreIesimo = counters[i];
+            let valoreContatore = parseInt(contatoreIesimo.innerHTML);
+            console.log(valoreContatore);
+            valoreContatore++;
+            contatoreIesimo.innerHTML = valoreContatore;
+        }
+    )
+})
+
+
+// document.getElementById("myCheckbox").addEventListener("click", 
+//         function(event){
+//             event.preventDefault()
+//         });
+
+
+// const likeIcon= document.querySelector(".like-button")
+// likeButton.addEventListener('click',changeColor)
+// function changeColor(){
+//     likeIcon.classList.add("like-button--liked")
+// }
+
+
+
+
+
+
+
